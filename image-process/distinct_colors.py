777b9d22591e8343e96df57ce9e6dd373af4e1cd
JackieLi <wq09680698@163.com>
2025-03-12 16:42:26 +0800

    add overlay specific label

---
+++ image-process/distinct_colors.py
@@ -0,0 +1,35 @@
+# Define a list of distinct colors (RGB values)
+distinct_colors = [
+    (1.0, 0.0, 0.0),  # Red
+    (0.0, 1.0, 0.0),  # Green
+    (0.0, 0.0, 1.0),  # Blue
+    (1.0, 1.0, 0.0),  # Yellow
+    (1.0, 0.0, 1.0),  # Magenta
+    (0.0, 1.0, 1.0),  # Cyan
+    (0.5, 0.0, 0.0),  # Dark Red
+    (0.0, 0.5, 0.0),  # Dark Green
+    (0.0, 0.0, 0.5),  # Dark Blue
+    (0.5, 0.5, 0.0),  # Olive
+    (0.5, 0.0, 0.5),  # Purple
+    (0.0, 0.5, 0.5),  # Teal
+    (0.75, 0.25, 0.0),  # Orange
+    (0.25, 0.75, 0.0),  # Lime
+    (0.0, 0.75, 0.25),  # Spring Green
+    (0.0, 0.25, 0.75),  # Azure
+    (0.25, 0.0, 0.75),  # Violet
+    (0.75, 0.0, 0.25),  # Rose
+    (1.0, 0.5, 0.5),  # Light Red
+    (0.5, 1.0, 0.5),  # Light Green
+    (0.5, 0.5, 1.0),  # Light Blue
+    (1.0, 0.5, 1.0),  # Light Magenta
+    (0.5, 1.0, 1.0),  # Light Cyan
+    (1.0, 1.0, 0.5),  # Light Yellow
+    (0.5, 0.0, 0.0),  # Maroon
+    (0.0, 0.5, 0.0),  # Dark Green
+    (0.0, 0.0, 0.5),  # Navy
+    (0.5, 0.5, 0.0),  # Olive
+    (0.5, 0.0, 0.5),  # Purple
+    (0.0, 0.5, 0.5),  # Teal
+    (1.0, 0.5, 0.0),  # Bright Orange
+    (0.5, 0.0, 1.0),  # Indigo
+]
--- image-process/visual-nii-image-mask-overlay-specific.py
+++ image-process/visual-nii-image-mask-overlay-specific.py
@@ -4,19 +4,32 @@ from distinct_colors import distinct_colors
 '''
 This script demonstrates how to overlay a mask on top of an image using the vtkImageActor class.
 The mask image is displayed with a transparent background.
-The default slab thickness is image bounds
 The user can scroll through the slices of the original image using the 'w' and 's' keys.
-Once reslice image, the slab thickness is set to 0.5
+The user can specify a specific mask value to overlay on the image.
 '''
 
+# Set up the lookup table for different masks
+def setup_lookup_table():
+    lut = vtk.vtkLookupTable()
+    lut.SetNumberOfTableValues(33)  # 0 to 32 (33 values)
+    lut.SetRange(0, 32)
+    lut.Build()
+    # Very important to set the background color to transparent
+    lut.SetTableValue(0, 0, 0, 0, 0)  # Background color (transparent)
+
+    # Set the colors in the lookup table
+    for i in range(1, 33):
+        color = distinct_colors[i - 1]
+        lut.SetTableValue(i, color[0], color[1], color[2], 1.0)
+        # print(f"Color for mask {i}: {lut.GetTableValue(i)}")
+    return lut
+
 class ResliceCallback:
-    def __init__(self, reslice, reslice_mask, image_actor, max_slices):
+    def __init__(self, reslice, image_actor, max_slices):
         self.reslice = reslice
-        self.reslice_mask = reslice_mask
         self.image_actor = image_actor
         self.slice = 0
         self.max_slices = max_slices
-        self.slice_thickness = 0.5
 
     def execute(self, obj, event):
         key = obj.GetKeyCode()
@@ -35,11 +48,8 @@ class ResliceCallback:
         self.reslice.GetResliceAxes().MultiplyPoint(offset, offset)
         # print("Offset:", offset)
         self.reslice.SetResliceAxesOrigin(offset[0], offset[1], offset[2])
-        self.reslice.SetSlabThickness(self.slice_thickness) 
         self.reslice.Update()
-        self.reslice_mask.SetSlabThickness(self.slice_thickness) # set mask slab thickness
-        self.reslice_mask.Update()
-        self.image_actor.GetMapper().Update() # trigger pipeline update: render the new slice
+        self.image_actor.GetMapper().Update()
 
 
 # Read the original NIFTI file (.nii.gz)
@@ -52,6 +62,27 @@ reader_mask = vtk.vtkNIFTIImageReader()
 reader_mask.SetFileName("data/liver_57_multilabel.nii.gz")
 reader_mask.Update()
 
+# Threshold specific mask value
+specific_min_value = 1
+specific_max_value = 33
+threshold = vtk.vtkImageThreshold()
+threshold.SetInputConnection(reader_mask.GetOutputPort())
+threshold.ThresholdBetween(specific_min_value, specific_max_value)
+threshold.SetInValue(1)
+threshold.SetOutValue(0)
+threshold.Update()
+
+# mask specific value
+image_mask = vtk.vtkImageMask()
+image_mask.SetMaskedOutputValue(0)
+# method 1: SetInputConnection
+# image_mask.SetInputConnection(0, reader_mask.GetOutputPort())
+# image_mask.SetInputConnection(1, threshold.GetOutputPort())
+# method 2: SetInputData
+image_mask.SetInputData(reader_mask.GetOutput())
+image_mask.SetMaskInputData(threshold.GetOutput())
+image_mask.Update()
+
 # Reslice the image
 resliceAxes = vtk.vtkMatrix4x4()
 # sagittal view
@@ -60,30 +91,18 @@ resliceAxes.DeepCopy((0, 0, 1, 0,
                       0, 1, 0, 0,
                       0, 0, 0, 1))
 
-# slab thickness is the image bounds
-bounds = reader_image.GetOutput().GetBounds()
-slab_thickness = bounds[1] - bounds[0] # x-axis: sagittal view
-# slab_thickness = bounds[3] - bounds[2] # y-axis: coronal view
-# slab_thickness = bounds[5] - bounds[4] # z-axis: axial view
-
 # reslice image and mask share the same reslice axes
-reslice_image = vtk.vtkImageSlabReslice()
+reslice_image = vtk.vtkImageReslice()
 reslice_image.SetInputConnection(reader_image.GetOutputPort())
 reslice_image.SetOutputDimensionality(2)
-reslice_image.SetSlabModeToMax()
-reslice_image.SetSlabThickness(slab_thickness)
-reslice_image.SetAutoCropOutput(True)
 reslice_image.SetResliceAxes(resliceAxes)
 reslice_image.SetResliceAxesOrigin(reader_image.GetOutput().GetCenter())
-reslice_image.SetInterpolationModeToLinear()  # Cubic for image data
+reslice_image.SetInterpolationModeToCubic()  # Cubic for image data
 reslice_image.Update()
 
-reslice_mask = vtk.vtkImageSlabReslice()
-reslice_mask.SetInputConnection(reader_mask.GetOutputPort())
+reslice_mask = vtk.vtkImageReslice()
+reslice_mask.SetInputConnection(image_mask.GetOutputPort())
 reslice_mask.SetOutputDimensionality(2)
-reslice_mask.SetSlabModeToMax()
-reslice_mask.SetSlabThickness(slab_thickness)
-reslice_mask.SetAutoCropOutput(True)
 reslice_mask.SetResliceAxes(resliceAxes)
 reslice_mask.SetResliceAxesOrigin(reader_mask.GetOutput().GetCenter())
 reslice_mask.SetInterpolationModeToNearestNeighbor()  # Nearest neighbor for mask data
@@ -104,24 +123,10 @@ reslice_mask.Update()
 dimensions = reader_image.GetOutput().GetDimensions()
 max_slices = dimensions[2]
 
-# Set up the lookup table for different masks
-lut = vtk.vtkLookupTable()
-lut.SetNumberOfTableValues(33)  # 0 to 32 (33 values)
-lut.SetRange(0, 32)
-lut.Build()
-# Very important to set the background color to transparent
-lut.SetTableValue(0, 0, 0, 0, 0)  # Background color (transparent)
-
-# Set the colors in the lookup table
-for i in range(1, 33):
-    color = distinct_colors[i - 1]
-    lut.SetTableValue(i, color[0], color[1], color[2], 1.0)
-    # print(f"Color for mask {i}: {lut.GetTableValue(i)}")
-
 # Apply the lookup table to map scalar values to colors for the mask
 color_map = vtk.vtkImageMapToColors()
 color_map.SetInputConnection(reslice_mask.GetOutputPort())
-color_map.SetLookupTable(lut)
+color_map.SetLookupTable(setup_lookup_table())
 color_map.PassAlphaToOutputOn()
 color_map.SetOutputFormatToRGBA()
 color_map.Update()
@@ -150,13 +155,10 @@ renderer.AddActor(mask_actor)
 renderer.SetBackground(0.1, 0.1, 0.1)
 
 # Initialize the reslice callback for mouse scroll events
-reslice_callback_image = ResliceCallback(reslice_image, reslice_mask, image_actor, max_slices)
-# because they share the same reslice axes, we can use one callback for both
-# reslice_callback_mask = ResliceCallback(reslice_mask, mask_actor, max_slices)
+reslice_callback_image = ResliceCallback(reslice_image, image_actor, max_slices)
 
 # Attach the callback to the interactor
 interactor.AddObserver("KeyPressEvent", reslice_callback_image.execute)
-# interactor.AddObserver("KeyPressEvent", reslice_callback_mask.execute)
 
 # Initialize and start the rendering loop
 render_window.Render()
